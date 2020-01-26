984a927bfd9a32db05cbb820bd1270ad57c6374d
Jan Kostrzewa <jankostrzewa@users.noreply.github.com>
2020-01-26 18:39:40 +0100

    added ES5 and ES6 functions for fun

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function getApiData() {
+// ES5 XHR API
+function getApiDataES5() {
+    var xhr = new XMLHttpRequest();
+    xhr.onloadstart = function (evt) {
+        var element = document.querySelector('#result');
+        element.innerHTML = '<span>loading data...</span>';
+    }
+
+    xhr.onload = function (evt) {
+        var element = document.querySelector('#result');
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        element.innerHTML = JSON.stringify(data);
+    }
+    xhr.onerror = function(evt) {
+        var element = document.querySelector('#result');
+        element.innerHTML = evt;
+    }
+
+    xhr.open('GET', 'http://localhost:8000/api/weatherforecast');
+    xhr.send();
+}
+
+// ES6 fetch - promise API
+function getApiDataES6() {
     var element = document.querySelector('#result');
     element.innerHTML = '<span>loading data...</span>';
     fetch('http://localhost:8000/api/weatherforecast')
@@ -13,4 +37,19 @@ function getApiData() {
         .catch(error => {
             element.innerHTML = error;
         })
+}
+
+// ES7 async - await API
+async function getApiData7() {
+    var element = document.querySelector('#result');
+    element.innerHTML = '<span>loading data...</span>';
+    try {
+        const data = await fetch('http://localhost:8000/api/weatherforecast');
+        const json = await data.json();
+        console.log(json);
+        element.innerHTML = JSON.stringify(data);
+    }
+    catch (error) {
+        element.innerHTML = error;
+    }
 }
